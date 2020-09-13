5b82c70f290f1e14b7ea5d4c971cfa5a89195711
dude <asimalilatif@gmail.com>
2020-09-13 18:59:13 +0500

    errors etc on login

--- routes/new.js
+++ routes/new.js
@@ -1,11 +1,26 @@
 var express = require('express');
-const { route } = require('.');
+const { Db } = require('mongodb');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  if(!req.session.racer){
-    res.render('new')
+  res.render('new',{error: req.param('error')})
+})
+
+router.post('/', async function(req, res, next) {
+  if(req.body.username){
+    results = await db.collection('accounts').findOne({name: req.body.username})
+    console.log(results)
+    if(!results){
+      let re = /^\w+$/
+      var isValid = re.test(req.body.username)
+      if(isValid){
+        
+      }
+      else res.redirect('/new?error=Invalid Characters in input. Only A-Z, a-z, _, 0-9 allowed')
+    }
+    else res.redirect('/new?error=Username not available')
   }
+  else res.send('Username required')
 })
 
 module.exports = router
