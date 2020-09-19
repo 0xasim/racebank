7758f3aa9e5252362e3992ddc1b2f59bdf47a08e
dude <asimalilatif@gmail.com>
2020-09-19 14:19:04 +0500

    reverse of already existing username could lead to problems. added primary and seondry usernames check

--- routes/new.js
+++ routes/new.js
@@ -7,18 +7,21 @@ router.get('/', (req, res, next) => {
 
 router.post('/', async function(req, res, next) {
   if(req.body.username){
-    results = await db.collection('accounts').findOne({name: req.body.username})
-
-    if(!results){
+    // See if primary i-e entered username already exists
+    prim = await db.collection('accounts').findOne({name: req.body.username})
+    // Reversed username also shouldn't exist
+    sec = await db.collection('accounts').findOne({name: req.body.username.split('').reverse().join('')})
+    
+    if(!prim && !sec){
       let re = /^\w+$/
       var isValid = re.test(req.body.username)
       if(isValid && req.body.username.length<20){
         let inRes = await db.collection('accounts').insertMany([
-          { // Primary account, real username
+          { // Secondary account, reversed username
             name: req.body.username.split('').reverse().join(''), // Reversed username 
             amount: 1000
           },
-          { // Secondary account, reversed username
+          { // Primary account, real username
             name: req.body.username,  
             amount: 0
           }
